6becdb486f58c01bbad595c1d3d349add2e9bfe9
Anjana V U <103420152+anjanavu@users.noreply.github.com>
2023-11-29 16:04:31 +0530

    Update app.js

--- empoyee_backend/app.js
+++ empoyee_backend/app.js
@@ -2,14 +2,17 @@ const express=require('express')
 const mongoose=require('mongoose');
 const app=new express();
 const cors=require('cors')
-
+require('dotenv').config(); 
 app.use(express.json());
 app.use(cors());
 const adminRouter=require('./routes/adminRoute');
 app.use('/admin',adminRouter);
 const userRouter=require('./routes/userRoute');
 app.use('/user',userRouter)
-mongoose.connect('mongodb+srv://anjanavu2000:anjana2000@cluster0.trc3jzo.mongodb.net/Employee?retryWrites=true&w=majority')
+mongoose.connect(process.env.MONGODB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
 .then(() => {
     console.log("Connected to DB");
   })
@@ -18,8 +21,8 @@ mongoose.connect('mongodb+srv://anjanavu2000:anjana2000@cluster0.trc3jzo.mongodb
   });
 
   
+ const PORT = process.env.PORT || 3033;
 
-
-  app.listen(3033,()=>{
+  app.listen(PORT,()=>{
     console.log("listening to the port 3033")
 })
